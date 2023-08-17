60cde957ea4f6553421bf2d66f128eabc7f92253
Mohamed Khalifa <7mdiano@gmail.com>
2023-08-17 12:53:57 +0300

    add date for ergaa2

--- src/App.js
+++ src/App.js
@@ -3,28 +3,7 @@ import CountDownList from './components/CountDownList/CountDownList';
 import Footer from './components/Footer/Footer';
 import Nav from './components/Nav/Nav';
 
-const dates = [
-  {
-    title: 'التقديم',
-    date: '2023-08-07T06:00:00',
-  },
-  {
-    title: 'الكشف الطبي',
-    date: '2023-08-16T06:00:00',
-  },
-  {
-    title: 'الإرجة',
-    date: '2023-09-01T06:00:00',
-  },
-  {
-    title: 'التدريب',
-    date: '2023-09-15T06:00:00',
-  },
-  {
-    title: 'التجنيد',
-    date: '2023-09-30T06:00:00',
-  },
-];
+const dates = Array.from(require('./dates.json'))
 
 function App() {
   return (
